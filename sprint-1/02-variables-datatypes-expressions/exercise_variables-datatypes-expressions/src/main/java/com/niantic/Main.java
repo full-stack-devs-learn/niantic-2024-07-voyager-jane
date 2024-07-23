982e1d3de7429c6e4cf2c85c4759edc9d3ba7415
Jane Huynh <janelehuynh1999@gmail.com>
2024-07-23 16:03:28 -0700

    Working on Day 2 Exercise - Finished S1 & S2

--- sprint-1/02-variables-datatypes-expressions/exercise_variables-datatypes-expressions/src/main/java/com/niantic/Main.java
+++ sprint-1/02-variables-datatypes-expressions/exercise_variables-datatypes-expressions/src/main/java/com/niantic/Main.java
@@ -50,19 +50,34 @@ public class Main
         // 1. Declare a new variable to hold your name.
         // Variable name: name
         // Data Type: string (names/words are strings)
+        String name;
+        name = "Jane";
+
+        System.out.println("1) Name");
+        System.out.println("Name: " + name);
+        System.out.println();
 
 
+        // 2. Declare a variable to hold your myAge.
+        // Variable name: myAge
+        // Data Type: int (myAge is a whole number)
+        int age;
+        age = 25;
 
-        // 2. Declare a variable to hold your age.
-        // Variable name: age
-        // Data Type: int (age is a whole number)
-
+        System.out.println("2) Age");
+        System.out.println("Age: " + age);
+        System.out.println();
 
 
         // 3. Declare a variable that holds the first 5 digits of PI.
         // Variable name: pi
         // Data Type: double (pi is a number with decimal places)
+        double pi;
+        pi = 3.1415;
 
+        System.out.println("3) First 5 digits of PI");
+        System.out.println("PI: " + pi);
+        System.out.println();
 
 
         // 4. Declare a variable holds the price of
@@ -72,6 +87,12 @@ public class Main
 
         // NOTE: the decimal data type  is not automatically recognized
         // see lecture notes
+        double adultMealPrice;
+        adultMealPrice = 34.67;
+
+        System.out.println("4) Buffet Meal Price");
+        System.out.println("Adult Meal: " + adultMealPrice);
+        System.out.println();
     }
 
     public static void section02()
@@ -97,47 +118,89 @@ public class Main
 
         // Hint: You cannot use the same variable name
         // as a variable that you have used before.
+        String favSuperHero;
+        favSuperHero = "Spiderman";
 
+        System.out.println("5) Favorite Super Hero");
+        System.out.println("Name: " + favSuperHero);
+        System.out.println();
 
 
         // 6. Declare a variable to hold the value of
         // value of a test score. The test has 100
         // questions and each question is worth 1 point.
         // (You can choose the score)
+        int testScore;
+        testScore = 92;
 
+        System.out.println("6) Test Score");
+        System.out.println("Test Score: " + testScore + "/100");
+        System.out.println();
 
 
         // 7. Declare a variable that holds the current
         // population in the United States.
         // (search what the population is today)
+        double populationUSA;
+        populationUSA = 333300000;
 
+        System.out.println("7) Current USA Population");
+        System.out.println("Population of: " + populationUSA);
+        System.out.println();
 
 
         // 8. Declare a variable that holds the current
         // population in China.
         // (search what the population is today)
+        double populationChina;
+        populationChina = 1412000000;
 
+        System.out.println("8) Current China Population");
+        System.out.println("Population of: " + populationChina);
+        System.out.println();
 
 
         // 9. Declare a variable that holds the current
         // population in the world.
         // (search what the population is today)
+        long populationWorld;
+        populationWorld = 8123115050L;
 
-
+        System.out.println("9) Current World Population");
+        System.out.println("Population of: " + populationWorld);
+        System.out.println();
 
         // 10. Declare a variable that specifies whether
         // or not your mouse is wireless.
+        boolean wirelessMouse;
+        wirelessMouse = false;
 
+        System.out.println("10) Is My Mouse Wireless?");
+        System.out.println("Answer: " + wirelessMouse);
+        System.out.println();
 
 
         // 11. Search what the Latitude and Longitude of your
         // home town are. Then declare 2 variables
         // to store the Latitude and Longitude.
+        double latitude = 47.9790;
+        double longitude = -122.2021;
 
+        System.out.println("11) Coordinates of My Hometown");
+        System.out.println("Latitude: " + latitude);
+        System.out.println("Longitude: " + longitude);
+        System.out.println();
 
 
         // 12. Create a variable that holds the current
         // Microsoft stock price.
+        double microsoftStock;
+        microsoftStock = 444.85;
+
+        System.out.println("12) Current Microsoft Stock Price");
+        System.out.println("Price: " + microsoftStock);
+        System.out.println();
+
     }
     
     public static void section03()
@@ -174,28 +237,61 @@ public class Main
         // but missed 3 of his shots.
 
         // How many shots did Pete take?
+        int successfulShots = 7;
+        int missedShots = 3;
 
+        int totalShots = successfulShots + missedShots;
+
+        System.out.println("13) Pete's Total Number of Shots");
+        System.out.println("Total: " + totalShots);
+        System.out.println();
 
 
         // 14. In his previous basketball game Pete took 20 shots.
         // He missed 6 shots, and he made 3 three point shots.
 
         // How many 2 point shots did Pete make?\
+        int previousTotalShots = 20;
+        int previousMissedShots = 6;
+        int threePointShots = 3;
 
+        int totalTwoPointShots = previousTotalShots - previousMissedShots - threePointShots;
+
+        System.out.println("14) Pete's Number of 2 Points Shots");
+        System.out.println("2 Point Shots: " + totalTwoPointShots);
+        System.out.println();
 
 
         // 15. Pete and Pat are teammates. Pete made 6 shots.
         // Pat made twice as many shots as Pete.
 
         // How many total shots did they make?
+        int peteShots = 6;
+        int patShots = peteShots * 2;
 
+        int totalPetePatShots = peteShots + patShots;
+
+        System.out.println("15) Pete and Pat's Total Number of Shots");
+        System.out.println("Total: " + totalPetePatShots);
+        System.out.println();
 
 
         // 16. Pete has made 13 shots, Pat has made 9.
         // If both Pete and Pat make 1 more shot each
         // before the game ends, how many total shots
         // did the Terrifying Twosome make in the game?
+        int peteShots2 = 13;
+        int patShots2 = 9;
+        int scoreAgain = 1;
 
+        int peteAgain = peteShots2 + scoreAgain;
+        int patAgain = patShots2 + scoreAgain;
+
+        int totalTerrifyingTwosome = peteAgain + patAgain;
+
+        System.out.println("16) Terrifying Twosome Total Shots");
+        System.out.println("Total: " + totalTerrifyingTwosome);
+        System.out.println();
 
 
         // 17. In his last game Pete made 11 shots.
