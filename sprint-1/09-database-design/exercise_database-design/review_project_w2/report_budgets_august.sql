4e4390825732a5afeb969c80928e8d253e0943e5
Jane Huynh <janelehuynh1999@gmail.com>
2024-08-02 15:55:05 -0700

    RP2/E9 - Added a little change in report script file

--- sprint-1/09-database-design/exercise_database-design/review_project_w2/report_budgets_august.sql
+++ sprint-1/09-database-design/exercise_database-design/review_project_w2/report_budgets_august.sql
@@ -9,6 +9,17 @@ WHERE MONTH(transaction_date) = 8
 GROUP BY category_id;
 
 
+-- Report on All Expenses in Categories By Month
+SELECT sub_categories.category_id
+, transaction_date
+, SUM(amount) 
+FROM transactions
+INNER JOIN sub_categories ON transactions.sub_category_id = sub_categories.sub_category_id
+GROUP BY category_id, transaction_date
+ORDER BY transaction_date;
+
+-- 
+
 -- Report on Expenses for a Category : Bills
 -- This is August expenses in Bills Category. Can test on July as Well.
 SELECT sub_category_name
@@ -18,11 +29,12 @@ INNER JOIN transactions on sub_categories.sub_category_id = transactions.sub_cat
 WHERE category_id = 2 AND MONTH(transaction_date) = 8
 GROUP BY sub_category_name;
 
--- This is all expenses in Bills Category
+-- This is all expenses in Bills Category Grouped by Months & SubCategoryName
 SELECT sub_category_name
 	, transaction_date
-	, amount
+	, SUM(amount)
 FROM sub_categories
 INNER JOIN transactions on sub_categories.sub_category_id = transactions.sub_category_id
 WHERE category_id = 2
+GROUP BY sub_category_name, transaction_date
 ORDER BY transaction_date;
