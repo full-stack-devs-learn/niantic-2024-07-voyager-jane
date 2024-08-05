d3f7bd488827e5f32756e0ccd1c323452d2a596c
Jane Huynh <janelehuynh1999@gmail.com>
2024-08-05 16:14:07 -0700

    Working on Challenges

--- sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_3_challenge/Hotel.java
+++ sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_3_challenge/Hotel.java
@@ -2,4 +2,57 @@ package com.niantic.part_3_challenge;
 
 public class Hotel
 {
+    private final int NUMBER_OF_ROOMS;
+    private final int NUMBER_OF_SUITES;
+    private int bookedRooms;
+    private int bookedSuites;
+
+    public Hotel(int numberOfRooms, int numberOfSuites)
+    {
+        this.NUMBER_OF_ROOMS = numberOfRooms;
+        this.NUMBER_OF_SUITES = numberOfSuites;
+    }
+
+    public Hotel(int numberOfRooms, int numberOfSuites, int bookedRooms, int bookedSuites)
+    {
+        this.NUMBER_OF_ROOMS = numberOfRooms;
+        this.NUMBER_OF_SUITES = numberOfSuites;
+        this.bookedRooms = bookedRooms;
+        this.bookedSuites = bookedSuites;
+    }
+
+    public int getNumberOfRooms()
+    {
+        return this.NUMBER_OF_ROOMS;
+    }
+
+    public int getNumberOfSuites()
+    {
+        return this.NUMBER_OF_SUITES;
+    }
+
+    public int getBookedRooms()
+    {
+        return this.bookedRooms;
+    }
+
+    public int getBookedSuites()
+    {
+        return this.bookedSuites;
+    }
+
+    public int getAvailableRooms()
+    {
+        return getNumberOfRooms() - getBookedRooms();
+    }
+
+    public int getAvailableSuites()
+    {
+        return getNumberOfSuites() - getBookedSuites();
+    }
+
+    public boolean makeReservation()
+    {
+        
+    }
 }
--- sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_3_challenge/Refrigerator.java
+++ sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_3_challenge/Refrigerator.java
@@ -2,4 +2,72 @@ package com.niantic.part_3_challenge;
 
 public class Refrigerator
 {
+    private final int MAX_CAPACITY;
+
+    private int currentTemperature;
+    private int availableCapacity;
+    private boolean isDoorOpen = false;
+
+    public Refrigerator(int currentTemperature, int maxCapacity)
+    {
+        this.currentTemperature = currentTemperature;
+        this.MAX_CAPACITY = maxCapacity;
+        this.availableCapacity = maxCapacity;
+    }
+
+    public int getCurrentTemperature()
+    {
+        return this.currentTemperature;
+    }
+
+    public int getMaxCapacity()
+    {
+        return this.MAX_CAPACITY;
+    }
+
+    public int getAvailableCapacity()
+    {
+        return this.availableCapacity;
+    }
+
+    public boolean isDoorOpen()
+    {
+        return this.isDoorOpen;
+    }
+
+    public void openDoor()
+    {
+        this.isDoorOpen = true;
+    }
+
+    public void closeDoor()
+    {
+        this.isDoorOpen = false;
+    }
+
+    public void addItem(int capacity)
+    {
+        if (isDoorOpen)
+        {
+            if (getAvailableCapacity() - capacity != 0)
+            {
+                this.availableCapacity -= capacity;
+            }
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    public void removeItem(int capacity)
+    {
+        if (isDoorOpen())
+        {
+            if (getAvailableCapacity() + capacity != this.MAX_CAPACITY)
+            {
+                this.availableCapacity += capacity;
+            }
+        }
+    }
 }
