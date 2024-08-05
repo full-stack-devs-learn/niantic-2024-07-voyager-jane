fbb981c98dbfba283a21a049bee6048e1a902dba
Jane Huynh <janelehuynh1999@gmail.com>
2024-08-05 14:30:14 -0700

    E11 - Passed test for Part 1

--- sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_1_sakila_dtos/Actor.java
+++ sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_1_sakila_dtos/Actor.java
@@ -2,4 +2,50 @@ package com.niantic.part_1_sakila_dtos;
 
 public class Actor
 {
+    private int actorID;
+    private String firstName;
+    private String lastName;
+
+    public Actor() {}
+
+    public Actor(int id, String firstName, String lastName)
+    {
+        this.actorID = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getActorId()
+    {
+        return this.actorID;
+    }
+
+    public void setActorId(int id)
+    {
+        this.actorID = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public String getFullName()
+    {
+        return firstName + " " + lastName;
+    }
 }
--- sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_1_sakila_dtos/Customer.java
+++ sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_1_sakila_dtos/Customer.java
@@ -2,4 +2,135 @@ package com.niantic.part_1_sakila_dtos;
 
 public class Customer
 {
+    private int customerId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private String country;
+    private String phone;
+
+    public Customer() {}
+
+    public Customer(int id, String firstName, String lastName, String email, String address, String city, String state, String zip, String country, String phone)
+    {
+        this.customerId = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.country = country;
+        this.phone = phone;
+    }
+
+    public int getCustomerId()
+    {
+        return this.customerId;
+    }
+
+    public void setCustomerId(int id)
+    {
+        this.customerId = id;
+    }
+
+    public String getFirstName()
+    {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public String getEmail()
+    {
+        return this.email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public String getAddress()
+    {
+        return this.address;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+
+    public String getCity()
+    {
+        return this.city;
+    }
+
+    public void setCity(String city)
+    {
+        this.city = city;
+    }
+
+    public String getState()
+    {
+        return this.state;
+    }
+
+    public void setState(String state)
+    {
+        this.state = state;
+    }
+
+    public String getZip()
+    {
+        return this.zip;
+    }
+
+    public void setZip(String zip)
+    {
+        this.zip = zip;
+    }
+
+    public String getCountry()
+    {
+        return this.country;
+    }
+
+    public void setCountry(String country)
+    {
+        this.country = country;
+    }
+
+    public String getPhone()
+    {
+        return this.phone;
+    }
+
+    public void setPhone(String phone)
+    {
+        this.phone = phone;
+    }
+
+    public String getFullName()
+    {
+        return firstName + " " + lastName;
+    }
 }
