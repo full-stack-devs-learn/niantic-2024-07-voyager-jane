a2ca88d49363bbb52e25054001d1a48742d81003
Jane Huynh <janelehuynh1999@gmail.com>
2024-08-13 15:24:34 -0700

    E17 - completed functions for CategoryDao and Finished index.html

--- sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/controllers/CategoriesController.java
+++ sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/controllers/CategoriesController.java
@@ -1,10 +1,36 @@
 package com.niantic.controllers;
 
+import com.niantic.models.Category;
 import com.niantic.services.CategoryDao;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
 
 @Controller
 public class CategoriesController
 {
     CategoryDao categoryDao = new CategoryDao();
+
+    @GetMapping("/categories")
+    public String getAllCategories(Model model)
+    {
+        ArrayList<Category> categories = categoryDao.getCategories();
+
+        model.addAttribute("categories", categories);
+
+        return "categories/index";
+    }
+
+    @GetMapping("/categories/{id}")
+    public String details(Model model, @PathVariable int id)
+    {
+        Category category = categoryDao.getCategoryById(id);
+
+        model.addAttribute("category", category);
+
+        return "categories/details";
+    }
 }
