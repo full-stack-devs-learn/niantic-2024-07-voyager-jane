a96e83ff88cd07321ca36fbd741f76b49bdc2a79
Jane Huynh <janelehuynh1999@gmail.com>
2024-08-14 13:22:57 -0700

    E17 - Attempted More of ProductController

--- sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/controllers/ProductsController.java
+++ sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/controllers/ProductsController.java
@@ -1,18 +1,31 @@
 package com.niantic.controllers;
 
+import com.niantic.models.Category;
+import com.niantic.models.Product;
+import com.niantic.services.CategoryDao;
 import com.niantic.services.ProductDao;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
 
 @Controller
 public class ProductsController
 {
     ProductDao productDao = new ProductDao();
+    CategoryDao categoryDao = new CategoryDao();
 
-    @GetMapping("/products")
-    public String getAllProducts(Model model) {
+    @GetMapping("/products?catId={id}")
+    public String getAllProductsByCategory(Model model, @PathVariable int id) {
 
-        return null;
+        ArrayList<Product> products = productDao.getProductsByCategory(id);
+        model.addAttribute("products", products);
+
+        Category category = categoryDao.getCategoryById(id);
+        model.addAttribute("category", category);
+
+        return "products/index";
     }
 }
