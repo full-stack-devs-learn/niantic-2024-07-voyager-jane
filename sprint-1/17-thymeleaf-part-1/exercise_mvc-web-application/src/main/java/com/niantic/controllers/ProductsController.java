c237396ed126c2d0e48b8a50911e1809fd1191c1
Jane Huynh <janelehuynh1999@gmail.com>
2024-08-14 08:15:00 -0700

    E17 - Starting ProductController

--- sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/controllers/ProductsController.java
+++ sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/controllers/ProductsController.java
@@ -1,5 +1,18 @@
 package com.niantic.controllers;
 
+import com.niantic.services.ProductDao;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ProductsController
 {
+    ProductDao productDao = new ProductDao();
+
+    @GetMapping("/products")
+    public String getAllProducts(Model model) {
+
+        return null;
+    }
 }
