abf59d1b7034b141af05934dd687040257741285
Jane Huynh <janelehuynh1999@gmail.com>
2024-08-14 15:30:34 -0700

    E18 - Completed Add, Edit, and Delete for Categories

--- sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/controllers/CategoriesController.java
+++ sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/controllers/CategoriesController.java
@@ -4,8 +4,7 @@ import com.niantic.models.Category;
 import com.niantic.services.CategoryDao;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -32,5 +31,57 @@ public class CategoriesController
         model.addAttribute("category", category);
 
         return "categories/details";
+    }
+
+    @GetMapping("/categories/add")
+    public String addCategory()
+    {
+        return "categories/add";
+    }
+
+    @PostMapping("/categories/add")
+    public String addCategory(@ModelAttribute("category") Category category)
+    {
+        categoryDao.addCategory(category);
+
+        return "redirect:/categories";
+    }
+
+    @GetMapping("/categories/{id}/edit")
+    public String editCategory(Model model, @PathVariable int id)
+    {
+        Category category = categoryDao.getCategoryById(id);
+
+        model.addAttribute("category", category);
+
+        return "categories/edit";
+    }
+
+    @PostMapping("/categories/{id}/edit")
+    public String editCategory(@ModelAttribute("category") Category category, @PathVariable int id)
+    {
+        category.setCategoryId(id);
+
+        categoryDao.updateCategory(category);
+
+        return "redirect:/categories";
+    }
+
+    @GetMapping("/categories/{id}/delete")
+    public String deleteCategory(Model model, @PathVariable int id)
+    {
+        Category category = categoryDao.getCategoryById(id);
+
+        model.addAttribute("category", category);
+
+        return "categories/delete";
+    }
+
+    @PostMapping("/categories/{id}/delete")
+    public String deleteCategory(@PathVariable int id)
+    {
+        categoryDao.deleteCategory(id);
+
+        return "redirect:/categories";
     }
 }
