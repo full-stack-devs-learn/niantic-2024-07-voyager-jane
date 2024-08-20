24b3c7ceef8ccb36183d35a062de0c6909afe8ce
Jane Huynh <janelehuynh1999@gmail.com>
2024-08-20 11:46:09 -0700

    S2 E3 - Finished Wizard Class Inheritance and Passed All Tests

--- sprint-2/02-inheritance/exercise_inheritance/src/main/java/com/niantic/Wizard.java
+++ sprint-2/02-inheritance/exercise_inheritance/src/main/java/com/niantic/Wizard.java
@@ -1,5 +1,59 @@
 package com.niantic;
 
-public class Wizard
+public class Wizard extends Character
 {
+    private int mana;
+
+    public Wizard(String name, int health, int level, int experience, int mana)
+    {
+        super(name, health, level, experience);
+
+        this.mana = mana;
+    }
+
+    public int getMana()
+    {
+        return mana;
+    }
+
+    public void castSpell(Character target)
+    {
+        if (!isDefeated() && mana >= 10) {
+            mana -= 10;
+            target.takeDamage(attackDamage * 2);
+            System.out.println(this.name + " attacks " + target.getName());
+        }
+
+        else
+        {
+            System.out.println(this.name + " has been defeated and cannot attack another player.");
+        }
+    }
+
+    public void regenerateMana(int amount)
+    {
+        if (isDefeated())
+        {
+            System.out.println(this.name + " has been defeated and cannot regenerate mana.");
+            return;
+        }
+
+        mana += amount;
+
+        System.out.println(this.name + " regenerated " + amount +  " mana and now has " + mana + " mana.");
+    }
+
+    @Override
+    public void levelUp()
+    {
+        super.levelUp();
+
+        if (!isDefeated()) regenerateMana(10);
+    }
+
+    @Override
+    public String specialAbility()
+    {
+        return "Cast Spells";
+    }
 }
