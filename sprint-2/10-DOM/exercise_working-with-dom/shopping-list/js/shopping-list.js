10701c2922229dd7377c69ad831c13a2204fb834
Jane Huynh <janelehuynh1999@gmail.com>
2024-08-30 15:35:13 -0700

    S2 E10 - Created Grocery List and Finished Button

--- sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-list.js
+++ sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-list.js
@@ -1,26 +1,104 @@
 // add pageTitle
-
 // add groceries
+const service = new ShoppingService();
+let pageTitle = service.getListName();
+let groceries = service.getShoppingList();
+
 
 /**
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
  */
-function displayListTitle() {}
+function displayListTitle() 
+{
+    const listTitle = document.getElementById("title");
+
+    listTitle.textContent = pageTitle;
+}
 
 /**
  * This function will loop over the array of groceries that was set above and add them to the DOM.
  */
-function displayGroceries() {}
+function displayGroceries() 
+{
+    const groceryContainer = document.getElementById("shopping-list");
+
+    groceries.forEach(item => 
+        {
+            createItemDiv(item, groceryContainer)
+        });
+
+
+}
+
+function createItemDiv(item, parent)
+{
+    const itemDiv = document.createElement("li");
+    itemDiv.classList.add("list-item");
+
+    parent.appendChild(itemDiv);
+
+    buildItemTitle(item, itemDiv);
+    buildQuantityDiv(item, itemDiv);
+}
+
+function buildItemTitle(item, parent)
+{
+    const titleDiv = document.createElement("div");
+    // titleDiv.classList.add("itemTitle");
+    titleDiv.textContent = item.title;
+
+    parent.appendChild(titleDiv);
+}
+
+function buildQuantityDiv(item, parent)
+{
+    const quantityDiv = document.createElement("div");
+    quantityDiv.classList.add("quantity-container");
+
+    parent.appendChild(quantityDiv);
+
+    buildQuantityText(item, quantityDiv);
+}
+
+function buildQuantityText(item, parent)
+{
+    const quantityTitle = document.createElement("p");
+    quantityTitle.classList.add("super");
+    quantityTitle.textContent = "quantity";
+
+    const quantityValue = document.createElement("p");
+    quantityValue.textContent = item.quantity;
+
+    parent.appendChild(quantityTitle);
+    parent.appendChild(quantityValue);
+}
 
 /**
  * This function will be called when the button is clicked. You will need to get a reference
  * to every list item and add the class completed to each one
  */
-function markCompleted() {
+function markCompleted() 
+{
+    const listItems = document.querySelectorAll(".list-item");
+
+    listItems.forEach(itemDiv => 
+        {
+            const divTitle = itemDiv.firstChild.textContent;
+
+            groceries.forEach(item => 
+                {
+                    if (item.title === divTitle && item.isComplete === true)
+                    {
+                        itemDiv.classList.add("complete");
+                    }
+                }); 
+        });
 }
 
-
-displayListTitle();
-displayGroceries();
+document.addEventListener("DOMContentLoaded", () => 
+{
+    displayListTitle();
+    displayGroceries();
+});
 
--- sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-service.js
+++ sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-service.js
@@ -1,13 +1,14 @@
 class ShoppingService {
 
     getListName() {
+        return "Shopping List";
     }
 
     getShoppingList() {
         return [
             {
                 id: 1,
-                title: '5 lb sack of potatoes',
+                title: 'Pickles',
                 quantity: 1,
                 isComplete: false
             },
@@ -15,6 +16,54 @@ class ShoppingService {
                 id: 2,
                 title: 'Frozen Pizza',
                 quantity: 4,
+                isComplete: true
+            },
+            {
+                id: 3,
+                title: 'Eggs - Dozen',
+                quantity: 1,
+                isComplete: false
+            },
+            {
+                id: 4,
+                title: 'Milk',
+                quantity: 1,
+                isComplete: true
+            },
+            {
+                id: 5,
+                title: 'Bread',
+                quantity: 2,
+                isComplete: false
+            },
+            {
+                id: 6,
+                title: 'Lettuce',
+                quantity: 1,
+                isComplete: true
+            },
+            {
+                id: 7,
+                title: 'Turkey Sandwich Meat',
+                quantity: 2,
+                isComplete: false
+            },
+            {
+                id: 8,
+                title: 'Tomatoes',
+                quantity: 4,
+                isComplete: true
+            },
+            {
+                id: 9,
+                title: 'Mayonnaise',
+                quantity: 1,
+                isComplete: false
+            },
+            {
+                id: 10,
+                title: 'Cheese',
+                quantity: 2,
                 isComplete: true
             }
         ];
