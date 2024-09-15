1e23ea607803c1995410f8a9aa353e72d788d519
Jane Huynh <janelehuynh1999@gmail.com>
2024-09-14 19:00:42 -0700

    S3 E1 - Passed Weapon Unit Tests

--- sprint-3/01-abstraction/exercise_abstraction/src/main/java/com/niantic/Weapon.java
+++ sprint-3/01-abstraction/exercise_abstraction/src/main/java/com/niantic/Weapon.java
@@ -1,5 +1,37 @@
 package com.niantic;
 
-public class Weapon
+public abstract class Weapon
 {
+    protected String name;
+    protected int damage;
+    protected int percentCharged;
+
+    public Weapon(String name, int damage)
+    {
+        this.name = name;
+        this.damage = damage;
+        this.percentCharged = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public int getPercentCharged() {
+        return percentCharged;
+    }
+
+    protected void setPercentCharged(int percentCharged) {
+        this.percentCharged = percentCharged;
+    }
+
+    public abstract int attack();
+
+    public abstract int powerAttack();
+
+    public abstract int getRange();
 }
