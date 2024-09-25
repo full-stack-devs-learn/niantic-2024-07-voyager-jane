a9ac4564cdcbce5173e23d52f6bff924643c8931
Jane Huynh <janelehuynh1999@gmail.com>
2024-09-24 18:00:58 -0700

    S3 E10 - Created Header Component

---
+++ sprint-3/10-introduction-to-react/exercise_northwind-react/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+export default function Header()
+{
+    return (
+        <div id="navigation">
+        <img src="images/logo.png" alt="Northwind Logo" />
+        <h1>Northwind Traders</h1>
+      </div>
+    )
+}
