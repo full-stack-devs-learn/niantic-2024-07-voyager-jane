203f2f80f2056c37ec584671304b0e4f73d775e2
Jane Huynh <janelehuynh1999@gmail.com>
2024-09-25 13:52:29 -0700

    S3 E10 - EmployeesRow Mapping

--- sprint-3/10-introduction-to-react/exercise_northwind-react/src/components/employees/employees-container/EmployeesContainer.jsx
+++ sprint-3/10-introduction-to-react/exercise_northwind-react/src/components/employees/employees-container/EmployeesContainer.jsx
@@ -1,13 +1,19 @@
 import './EmployeesContainer.css'
 import EmployeesRow from '../employees-row/EmployeesRow'
+import { employees } from '../../../data'
 
 export default function EmployeesContainer()
 {
     return (
         <main className="container mt-4 employees-container" id="employees-container">
-
-            <EmployeesRow></EmployeesRow>
-
+        {
+            employees.map((employee) => (
+                <EmployeesRow key={employee.employeeId}
+                            fullName={employee.firstName + " " + employee.lastName}
+                            title={employee.title}
+                            id={employee.employeeId}></EmployeesRow>
+            ))
+        }
         </main>
     )
 }
--- sprint-3/10-introduction-to-react/exercise_northwind-react/src/components/employees/employees-row/EmployeesRow.jsx
+++ sprint-3/10-introduction-to-react/exercise_northwind-react/src/components/employees/employees-row/EmployeesRow.jsx
@@ -1,72 +1,82 @@
 import './EmployeesRow.css'
 
-export default function EmployeesRow()
+export default function EmployeesRow({fullName, title, id})
 {
+    const imageUrl = `images/employees/${id}.webp`
+
     return (
         <>
         <div className="employee-row">
-            <img id="employee-image" src="images/employees/1.webp" />
-            <div className="employee-name">
-                <h1>Nancy Davolio</h1>
-                <h6>Sales Representative</h6>
-            </div>
-            </div>
-            <div className="employee-row">
-            <img id="employee-image" src="images/employees/2.webp" />
-            <div className="employee-name">
-                <h1>Andrew Fuller</h1>
-                <h6>Vice President, Sales</h6>
-            </div>
-            </div>
-            <div className="employee-row">
-            <img id="employee-image" src="images/employees/3.webp" />
-            <div className="employee-name">
-                <h1>Janet Leverling</h1>
-                <h6>Sales Representative</h6>
-            </div>
-            </div>
-            <div className="employee-row">
-            <img id="employee-image" src="images/employees/4.webp" />
-            <div className="employee-name">
-                <h1>Margaret Peacock</h1>
-                <h6>Sales Representative</h6>
-            </div>
-            </div>
-            <div className="employee-row">
-            <img id="employee-image" src="images/employees/5.webp" />
-            <div className="employee-name">
-                <h1>Steven Buchanan</h1>
-                <h6>Sales Manager</h6>
-            </div>
-            </div>
-            <div className="employee-row">
-            <img id="employee-image" src="images/employees/6.webp" />
-            <div className="employee-name">
-                <h1>Michael Suyama</h1>
-                <h6>Sales Representative</h6>
-            </div>
-            </div>
-            <div className="employee-row">
-            <img id="employee-image" src="images/employees/7.webp" />
-            <div className="employee-name">
-                <h1>Robert King</h1>
-                <h6>Sales Representative</h6>
-            </div>
-            </div>
-            <div className="employee-row">
-            <img id="employee-image" src="images/employees/8.webp" />
-            <div className="employee-name">
-                <h1>Laura Callahan</h1>
-                <h6>Inside Sales Coordinator</h6>
-            </div>
-            </div>
-            <div className="employee-row">
-            <img id="employee-image" src="images/employees/9.webp" />
-            <div className="employee-name">
-                <h1>Anne Dodsworth</h1>
-                <h6>Sales Representative</h6>
-            </div>
-            </div>
+        <img id="employee-image" src={imageUrl} />
+        <div className="employee-name">
+            <h1>{fullName}</h1>
+            <h6>{"Employee Id: " + id}</h6>
+            <h6>{title}</h6>
+        </div>
+        </div>
+        {/* <div className="employee-row">
+        <img id="employee-image" src="images/employees/1.webp" />
+        <div className="employee-name">
+            <h1>Nancy Davolio</h1>
+            <h6>Sales Representative</h6>
+        </div>
+        </div>
+        <div className="employee-row">
+        <img id="employee-image" src="images/employees/2.webp" />
+        <div className="employee-name">
+            <h1>Andrew Fuller</h1>
+            <h6>Vice President, Sales</h6>
+        </div>
+        </div>
+        <div className="employee-row">
+        <img id="employee-image" src="images/employees/3.webp" />
+        <div className="employee-name">
+            <h1>Janet Leverling</h1>
+            <h6>Sales Representative</h6>
+        </div>
+        </div>
+        <div className="employee-row">
+        <img id="employee-image" src="images/employees/4.webp" />
+        <div className="employee-name">
+            <h1>Margaret Peacock</h1>
+            <h6>Sales Representative</h6>
+        </div>
+        </div>
+        <div className="employee-row">
+        <img id="employee-image" src="images/employees/5.webp" />
+        <div className="employee-name">
+            <h1>Steven Buchanan</h1>
+            <h6>Sales Manager</h6>
+        </div>
+        </div>
+        <div className="employee-row">
+        <img id="employee-image" src="images/employees/6.webp" />
+        <div className="employee-name">
+            <h1>Michael Suyama</h1>
+            <h6>Sales Representative</h6>
+        </div>
+        </div>
+        <div className="employee-row">
+        <img id="employee-image" src="images/employees/7.webp" />
+        <div className="employee-name">
+            <h1>Robert King</h1>
+            <h6>Sales Representative</h6>
+        </div>
+        </div>
+        <div className="employee-row">
+        <img id="employee-image" src="images/employees/8.webp" />
+        <div className="employee-name">
+            <h1>Laura Callahan</h1>
+            <h6>Inside Sales Coordinator</h6>
+        </div>
+        </div>
+        <div className="employee-row">
+        <img id="employee-image" src="images/employees/9.webp" />
+        <div className="employee-name">
+            <h1>Anne Dodsworth</h1>
+            <h6>Sales Representative</h6>
+        </div>
+        </div> */}
         </>
     )
 }
