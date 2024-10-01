ffad6277914762b295d03b98959a50552e98648f
Jane Huynh <janelehuynh1999@gmail.com>
2024-10-01 01:12:18 -0700

    S3 E13 - Pokemon API Start

---
+++ sprint-3/13-typescript/exercise/pokemon-react-ts/src/services/pokemon-service.tsx
@@ -0,0 +1,35 @@
+// define the SPokeAPI Pokemon service logic here
+
+class PokemonService
+{
+    pageNum = 0;
+    baseUrl = `${config.baseUrl}/pokemon`
+
+    // write a function that allows you to request a page
+    // of 20 pokemon - how can you request page 1,2, 3 etc?
+    // consider using async/await with your axios request
+
+    async getPokemons(page, num)
+    {
+        let response;
+        this.pageNum = num * 20;
+
+        if (page)
+        {
+            response = await axios.get(`${this.baseUrl}?offset=${this.pageNum}&limit=20`);
+        }
+        else 
+        {
+            response = await axios.get(`${this.baseUrl}?offset=0&limit=20`);
+        }
+
+        return response.data;
+    }
+
+    async getPokemonByName(name)
+    {
+        const response = await axios.get(`${this.baseUrl}/${name}`);
+
+        return response.data;
+    }
+}
